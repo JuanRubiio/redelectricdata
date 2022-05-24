114ff20af916907fae1b0f9e433e1b1861156013
JuanRubiio <juan12_rubio@hotmail.com>
2022-05-24 23:44:10 +0200

    prueba de despliegue

--- src/crud.py
+++ src/crud.py
@@ -2,4 +2,5 @@ from sqlalchemy.orm import Session
 import model
 
 def getConsumo(db:Session):
-    return db.query(model.CONSUMO).order_by(model.CONSUMO.consumo_id).all()
+    #return db.query(model.CONSUMO).order_by(model.CONSUMO.id).all()
+    return "hello"
