4e9bf144a6a1530ad42f1217955bcfcddd53854f
JuanRubiio <juan12_rubio@hotmail.com>
2022-05-31 22:41:54 +0200

    funcionando en local con plantilla

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from typing import List
 import databases
 import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import sessionmaker
 from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
@@ -9,41 +12,25 @@ import os
 import urllib
 
 #DATABASE_URL = "sqlite:///./test.db"
+DB_HOST = 'tfmdb1.database.windows.net'
+DB_PORT = '1433'
+DB_DATABASE = 'tfmdb1'
+DB_USER = 'adm'
+DB_PASSWORD = 'Pa$$w0rd01'
+db_driver:str = "{ODBC Driver 17 for SQL Server}"
 
-host_server = os.environ.get('host_server', 'localhost')
-db_server_port = urllib.parse.quote_plus(str(os.environ.get('db_server_port', '5432')))
-database_name = os.environ.get('database_name', 'fastapi')
-db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', 'postgres')))
-db_password = urllib.parse.quote_plus(str(os.environ.get('db_password', 'secret')))
-ssl_mode = urllib.parse.quote_plus(str(os.environ.get('ssl_mode','prefer')))
-DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}?sslmode={}'.format(db_username, db_password, host_server, db_server_port, database_name, ssl_mode)
 
-database = databases.Database(DATABASE_URL)
+conn = f"""Driver={db_driver};Server=tcp:{DB_HOST},{DB_PORT};Database={DB_DATABASE};Uid={DB_USER};Pwd={DB_PASSWORD};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"""
 
-metadata = sqlalchemy.MetaData()
+params = urllib.parse.quote_plus(conn)
+conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
 
-notes = sqlalchemy.Table(
-    "notes",
-    metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("text", sqlalchemy.String),
-    sqlalchemy.Column("completed", sqlalchemy.Boolean),
-)
+engine_midb_dev = create_engine(conn_str, echo=True)
+Session_mi_dev = sessionmaker(autocommit=False, autoflush=False, bind=engine_midb_dev)
 
-engine = sqlalchemy.create_engine(
-    #DATABASE_URL, connect_args={"check_same_thread": False}
-    DATABASE_URL, pool_size=3, max_overflow=0
-)
-metadata.create_all(engine)
+Base_data = automap_base()
+Base_data.prepare(engine_midb_dev, reflect =True, schema='dbo')
 
-class NoteIn(BaseModel):
-    text: str
-    completed: bool
-
-class Note(BaseModel):
-    id: int
-    text: str
-    completed: bool
 
 app = FastAPI(title="REST API using FastAPI PostgreSQL Async EndPoints")
 app.add_middleware(
@@ -55,38 +42,6 @@ app.add_middleware(
 )
 app.add_middleware(GZipMiddleware)
 
-@app.on_event("startup")
-async def startup():
-    await database.connect()
-
-@app.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
-
-@app.post("/notes/", response_model=Note, status_code = status.HTTP_201_CREATED)
-async def create_note(note: NoteIn):
-    query = notes.insert().values(text=note.text, completed=note.completed)
-    last_record_id = await database.execute(query)
-    return {**note.dict(), "id": last_record_id}
-
-@app.put("/notes/{note_id}/", response_model=Note, status_code = status.HTTP_200_OK)
-async def update_note(note_id: int, payload: NoteIn):
-    query = notes.update().where(notes.c.id == note_id).values(text=payload.text, completed=payload.completed)
-    await database.execute(query)
-    return {**payload.dict(), "id": note_id}
-
-@app.get("/notes/", response_model=List[Note], status_code = status.HTTP_200_OK)
+@app.get("/hello/", status_code = status.HTTP_200_OK)
 async def read_notes(skip: int = 0, take: int = 20):
-    query = notes.select().offset(skip).limit(take)
-    return await database.fetch_all(query)
-
-@app.get("/notes/{note_id}/", response_model=Note, status_code = status.HTTP_200_OK)
-async def read_notes(note_id: int):
-    query = notes.select().where(notes.c.id == note_id)
-    return await database.fetch_one(query)
-
-@app.delete("/notes/{note_id}/", status_code = status.HTTP_200_OK)
-async def delete_note(note_id: int):
-    query = notes.delete().where(notes.c.id == note_id)
-    await database.execute(query)
-    return {"message": "Note with id: {} deleted successfully!".format(note_id)}
+    return 'HELLO'
